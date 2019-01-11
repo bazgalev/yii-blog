69d1722f626be1d65ae41b2a370abc1f8008d146
bazgalev <pazgalevm@gmail.com>
2019-01-12 02:43:10 +0300

    Change email on username

--- views/auth/login.php
+++ views/auth/login.php
@@ -19,12 +19,9 @@ $this->params['breadcrumbs'][] = $this->title;
                 <span class="border border-primary"></span>
                 <form method="post" style="box-sizing:border-box;border:1px solid #333333;padding:30px;margin-bottom: 50px;border-radius: 15px;">
                     <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                               placeholder="Enter email">
-                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
-                            else.
-                        </small>
+                        <label for="exampleInputName">User name</label>
+                        <input type="text" class="form-control" id="exampleInputName" placeholder="Enter username">
+
                     </div>
                     <div class="form-group">
                         <label for="exampleInputPassword1">Password</label>
