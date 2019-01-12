589c59ff6158b44d90bef92c543cb602a2695009
bazgalev <pazgalevm@gmail.com>
2019-01-12 03:09:50 +0300

    Added csrf token in form

--- views/auth/login.php
+++ views/auth/login.php
@@ -17,20 +17,24 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="row">
             <div class="col-md-4 col-md-offset-4">
                 <span class="border border-primary"></span>
-                <form method="post" style="box-sizing:border-box;border:1px solid #333333;padding:30px;margin-bottom: 50px;border-radius: 15px;">
+                <form method="post" action="/auth/login"
+                      style="box-sizing:border-box;border:1px solid #333333;padding:30px;margin-bottom: 50px;border-radius: 15px;">
                     <div class="form-group">
                         <label for="exampleInputName">User name</label>
-                        <input type="text" class="form-control" id="exampleInputName" placeholder="Enter username">
+                        <input type="text" class="form-control" id="exampleInputName" name="username"
+                               placeholder="Enter username">
 
                     </div>
                     <div class="form-group">
                         <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        <input type="password" name="password" class="form-control" id="exampleInputPassword1"
+                               placeholder="Password">
                     </div>
                     <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <input type="checkbox" name="rememberMe" class="form-check-input" id="exampleCheck1">
                         <label class="form-check-label" for="exampleCheck1">Check me out</label>
                     </div>
+                    <input type="hidden" name="_csrf" value="<?= Yii::$app->request->getCsrfToken() ?>"/>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
             </div>
