a379005a15cadbc0ceae37d8c044753d127650ab
bazgalev <pazgalevm@gmail.com>
2019-01-15 00:34:10 +0300

    login view update

--- views/auth/login.php
+++ views/auth/login.php
@@ -15,29 +15,25 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="main-content">
     <div class="container">
         <div class="row">
-            <div class="col-md-5 col-md-offset-4 m-b-lg">
-
+            <div class="col-md-5 col-md-offset-3 m-b-lg">
 
                 <div class="bordered-form">
-                <h2>Login form</h2>
+                    <h2>Login form</h2>
+                    <small>Please fill the following fields to login</small>
 
-                <?php $form = ActiveForm::begin([
-//                    'options' => [
-//                        'class' => 'bordered-form'
-//                    ],
-                ]); ?>
+                    <?php $form = ActiveForm::begin(); ?>
 
-                <?= $form->field($model, 'name') ?>
+                    <?= $form->field($model, 'name'); ?>
 
-                <?= $form->field($model, 'password')->passwordInput(); ?>
+                    <?= $form->field($model, 'password')->passwordInput(); ?>
 
-                <?= $form->field($model, 'rememberMe')->checkbox(); ?>
+                    <?= $form->field($model, 'rememberMe')->checkbox(); ?>
 
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary']) ?>
-                </div>
+                    <div class="form-group">
+                        <?= Html::submitButton('Login', ['class' => 'btn btn-primary']) ?>
+                    </div>
 
-                <?php ActiveForm::end(); ?>
+                    <?php ActiveForm::end(); ?>
                 </div>
             </div>
         </div>
