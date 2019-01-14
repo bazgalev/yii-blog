137bea8f49c3fc4bccfeac5f4ee5a5c2f25ac5fd
bazgalev <pazgalevm@gmail.com>
2019-01-15 00:40:07 +0300

    First version of signup view

---
+++ views/auth/sign-up.php
@@ -0,0 +1,43 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+
+/* @var $model app\models\LoginForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+$this->title = 'Sign up';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<div class="main-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 col-md-offset-3 m-b-lg">
+
+                <div class="bordered-form">
+                    <h2>Sign up form</h2>
+                    <small>Please fill the following fields to sign up</small>
+
+                    <?php $form = ActiveForm::begin(); ?>
+
+                    <?= $form->field($model, 'name'); ?>
+
+                    <?= $form->field($model, 'email'); ?>
+
+                    <?= $form->field($model, 'password')->passwordInput(); ?>
+
+                    <?= $form->field($model, 'rememberMe')->checkbox(); ?>
+
+                    <div class="form-group">
+                        <?= Html::submitButton('Login', ['class' => 'btn btn-primary']) ?>
+                    </div>
+
+                    <?php ActiveForm::end(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
